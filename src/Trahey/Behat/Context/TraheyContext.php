3eaff76303108abd9d607a5ed86decb9029307da
Chris Trahey <christrahey@gmail.com>
2012-10-14 23:18:18 -0700

    Adding namespacing and autoload key to composer.json

---
+++ src/Trahey/Behat/Context/TraheyContext.php
@@ -0,0 +1,13 @@
+<?php
+namespace Trahey\Behat\Context;
+use Drupal\DrupalExtension\Context\DrupalContext;
+require_once 'PHPUnit/Autoload.php';
+require_once 'PHPUnit/Framework/Assert/Functions.php';
+
+require_once 'PHPUnit/Autoload.php';
+require_once 'PHPUnit/Framework/Assert/Functions.php';
+
+
+class TraheyContext extends DrupalContext {
+  
+}
