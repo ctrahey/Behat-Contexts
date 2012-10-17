7e29ce90bbbc17fce389006350027a20ebf02153
Chris Trahey <christrahey@gmail.com>
2012-10-16 23:58:02 -0700

    A working prototype

---
+++ src/Trahey/Behat/Context/EmailContext.php
@@ -0,0 +1,23 @@
+<?php
+namespace Trahey\Behat\Context;
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+/**
+ * Email context.
+ */
+class EmailContext extends BehatContext
+{
+  /**
+     * @Given /^I have an empty inbox$/
+     */
+    public function iHaveAnEmptyInbox() {
+      throw new PendingException();
+    }
+  
+  
+}
--- src/Trahey/Behat/Context/TraheyContext.php
+++ src/Trahey/Behat/Context/TraheyContext.php
@@ -9,24 +9,31 @@ require_once 'PHPUnit/Framework/Assert/Functions.php';
 
 
 class TraheyContext extends DrupalContext {
+  
+  /**
+   * @beforeScenario @email
+   */
+  public function attachEmailContext($event) {
+    $email = new EmailContext();
+    $this->useContext('mtm_email', $email);
+  }
+  
+  
   /**
     * @Given /^I have browser which is supported$/
     */
    public function iHaveBrowserWhichIsSupported() {
-     throw new PendingException();
    }
 
    /**
     * @When /^I visit the site$/
     */
    public function iVisitTheSite() {
-     throw new PendingException();
    }
 
    /**
     * @Then /^I should get a valid page$/
     */
    public function iShouldGetAValidPage() {
-     throw new PendingException();
    }  
 }
