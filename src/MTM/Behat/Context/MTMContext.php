8f2fd138de6e430b297c4d88fd4ef006fd9fba15
Kronda <ephany@gmail.com>
2012-10-19 15:35:29 -0700

    Update src/MTM/Behat/Context/MTMContext.php
    Got rid of double require statements.

--- src/MTM/Behat/Context/MTMContext.php
+++ src/MTM/Behat/Context/MTMContext.php
@@ -4,10 +4,6 @@ use Drupal\DrupalExtension\Context\DrupalContext;
 require_once 'PHPUnit/Autoload.php';
 require_once 'PHPUnit/Framework/Assert/Functions.php';
 
-require_once 'PHPUnit/Autoload.php';
-require_once 'PHPUnit/Framework/Assert/Functions.php';
-
-
 class MTMContext extends DrupalContext {
   
   /**
