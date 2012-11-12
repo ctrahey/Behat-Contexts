045d80aef62c47a07814c3a8dce2b24eb29cf146
Chris Trahey <christrahey@gmail.com>
2012-11-11 23:27:15 -0800

    Removing if(method_exists()) from SubContext::__call()
    This is because we added dynamic __call() to main Context, which
    means method_exists() == FALSE will not necessarily indicate a lack
    of callability.

--- src/MTM/Behat/SubContext.php
+++ src/MTM/Behat/SubContext.php
@@ -3,9 +3,6 @@ namespace MTM\Behat;
 
 class SubContext extends \Behat\Behat\Context\BehatContext {
   public function __call($method, $args) {
-    if(method_exists($this->getMainContext(), $method)) {
-      return call_user_func_array(array($this->getMainContext(), $method), $args);
-    }
-    return parent::__call($method, $args);
+    return call_user_func_array(array($this->getMainContext(), $method), $args);
   } 
 }
