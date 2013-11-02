e54c1492ffb48fa3093a55e05aeed71a227a376c
Jonathan Jordan <jonathan@metaltoad.com>
2013-11-02 12:25:49 -0700

    Allow creating users with known names and roles. Example: Given users: | name   | status | roleIds | | user1  | 1      | 3       | | user2  | 1      | 5       |

--- src/MTM/Behat/Wrappers/DrupalContext.php
+++ src/MTM/Behat/Wrappers/DrupalContext.php
@@ -2,6 +2,8 @@
 
 namespace MTM\Behat\Wrappers;
 
+use Drupal\DrupalExtension\Event\EntityEvent;
+
 /**
  * This class is a hack.
  * It is designed solely to allow calling protected methods from elsewhere.
@@ -9,11 +11,25 @@ namespace MTM\Behat\Wrappers;
  * in a composition pattern the same way you would with extension.
  */
 class DrupalContext extends \Drupal\DrupalExtension\Context\DrupalContext {
-  
+
   public function __call($method, $args) {
     if(method_exists($this, $method)) {
       return call_user_func_array(array($this, $method), $args);
     }
   }
-  
+
+
+  /**
+   * @beforeUserCreate
+   *
+   * Note: This only works when using the @api driver and not the @drush driver.
+   */
+  public function setRoles(EntityEvent $event) {
+    $user = $event->getEntity();
+    if (!empty($user->roleIds)) {
+      $user->roles = explode(',', $user->roleIds);
+      unset($user->roleIds);
+    }
+  }
+
 }
